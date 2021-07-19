770087ff51e71e065b52e97e11088e84caa92446
educartj <educartj@gmail.com>
2021-07-19 14:19:42 -0300

    Conceitos do bk

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
 import express from 'express';
-import { categoriesRoutes } from './routes/categories.routes';
+
+import swaggerUi from 'swagger-ui-express';
+
+import swaggerFiles from './swagger.json';
+
+import { router } from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/categories', categoriesRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFiles));
+
+app.use(router);
 
 app.listen(3333, () => console.log('Server is running!'));
