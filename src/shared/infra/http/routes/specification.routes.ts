7d6b9b137012308e2d63386031f70464b1a748de
educartj <educartj@gmail.com>
2021-08-31 01:38:44 -0300

    Regras de Negocios, Test Jest, Supertest, docsApi

--- src/shared/infra/http/routes/specification.routes.ts
+++ src/shared/infra/http/routes/specification.routes.ts
@@ -1,14 +1,13 @@
 import { Router } from 'express';
 
 import { CreateSpecificationController } from '@modules/cars/useCases/createSpecification/CreateSpecificationController';
-import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated';
+import { ensureAdmin } from "@shared/infra/http/middlewares/ensureAdmin";
+import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
 
 const specificationsRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 
-specificationsRoutes.use(ensureAuthenticated);
-
-specificationsRoutes.post('/', createSpecificationController.handle);
+specificationsRoutes.post('/', ensureAdmin, ensureAuthenticated, createSpecificationController.handle);
 
 export { specificationsRoutes };
---
+++ src/shared/infra/typeorm/migrations/1629236580136-CreateUsersToken.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateUsersToken1629236580136 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
